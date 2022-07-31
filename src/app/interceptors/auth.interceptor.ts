0dc193078737c27dbb15a5999683d545b460d17a
Diamou Serigne Touba Ndiaye <dstndiaye@gmail.com>
2022-07-31 09:15:58 +0000

    Ajoue d'un intercepteur

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor( private authService: AuthService ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const headers = new HttpHeaders()
+    .append('Authorization', `Bearer ${this.authService.getToken()}`);
+  const modifiedReq = request.clone({ headers });
+  return next.handle(modifiedReq);
+
+
+  }
+}
