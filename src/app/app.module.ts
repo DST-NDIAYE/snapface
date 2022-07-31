0e6516a68baa8330290e76d750a4752805d866b8
Diamou Serigne Touba Ndiaye <dstndiaye@gmail.com>
2022-07-31 11:14:04 +0000

    deplacement des element dans cors modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,18 @@ import { HttpClientModule } from '@angular/common/http' ;
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
-import { httpInterceptorProviders } from './interceptors';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
-    HeaderComponent,
     LandingPageComponent,
     SingleFaceSnapComponent,
     NewFaceSnapComponent
@@ -30,17 +28,14 @@ import { httpInterceptorProviders } from './interceptors';
     AppRoutingModule ,
     FormsModule ,
     ReactiveFormsModule ,
-    HttpClientModule
+    HttpClientModule ,
+    CoreModule
 
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR' } ,
-    httpInterceptorProviders
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    registerLocaleData(fr.default);
-  }
+ 
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { httpInterceptorProviders } from './interceptors';
+import { RouterModule } from '@angular/router';
+import * as fr from '@angular/common/locales/fr';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent ,
+  ],
   imports: [
-    CommonModule
+    CommonModule ,
+    RouterModule
+  ] ,
+
+  exports: [
+    HeaderComponent
+  ],
+
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR' } ,
+    httpInterceptorProviders
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
